fcae3c9f6f63596ffde06eb9bb6c9d9e5c6696fd
Dinesh <141162981+DineshBokka@users.noreply.github.com>
2024-06-06 11:27:54 +0530

    Create Maincode.py

---
+++ Maincode.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("Which city you grow up in?\n")
+pet = input("what is the name of the pet\n")
+print("Your Band Name could be " + city " " + pet)
